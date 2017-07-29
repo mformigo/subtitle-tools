629d97429542be88cd5a092e428b088befaf0f3c
Sjors <sjorsottjes@gmail.com>
2017-07-29 17:55:06 +0200

    add git attributes

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 Route::get('/')->uses('HomeController@index')->name('home');
 
 Route::get('/convert-sub-idx-to-srt-online')->uses('SubIdxController@index')->name('sub-idx-page');
-Route::post('/convert-sub-idx-to-srt-online')->uses('SubIdxController@post')->name('sub-idx-page');
+Route::post('/convert-sub-idx-to-srt-online')->uses('SubIdxController@post');
