17c782290fa7a30bbb696d00b71b74257a903b7f
Sjors <sjorsottjes@gmail.com>
2017-08-08 18:08:20 +0200

    improve routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,9 @@
 
 Route::get('/')->uses('HomeController@index')->name('home');
 
-Route::get('/convert-sub-idx-to-srt-online')->uses('SubIdxController@index')->name('sub-idx-index');
-Route::post('/convert-sub-idx-to-srt-online')->uses('SubIdxController@post');
-Route::get('/convert-sub-idx-to-srt-online/{pageId}')->uses('SubIdxController@detail')->name('sub-idx-detail');
-Route::get('/convert-sub-idx-to-srt-online/{pageId}/{index}')->uses('SubIdxController@downloadSrt')->name('sub-idx-dl');
+Route::prefix('convert-sub-idx-to-srt-online')->group(function() {
+    Route::get('/')->uses('SubIdxController@index')->name('sub-idx-index');
+    Route::post('/')->uses('SubIdxController@post');
+    Route::get('/{pageId}')->uses('SubIdxController@detail')->name('sub-idx-detail');
+    Route::post('/{pageId}/{index}')->uses('SubIdxController@downloadSrt')->name('sub-idx-dl');
+});
