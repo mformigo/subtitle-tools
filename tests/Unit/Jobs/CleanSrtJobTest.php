19250b9b9560449feb514898da206b59ecfcc4e1
Sjors <sjorsottjes@gmail.com>
2017-08-15 13:54:20 +0200

    add clean srt tests

---
+++ tests/Unit/Jobs/CleanSrtJobTest.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Facades\TextFileFormat;
+use App\Jobs\CleanSrtJob;
+use App\Models\FileJob;
+use App\Models\StoredFile;
+use App\Subtitles\PlainText\Srt;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\CreatesFileGroups;
+use Tests\TestCase;
+
+class CleanSrtJobTest extends TestCase
+{
+    use DatabaseMigrations, CreatesFileGroups;
+
+    /** @test */
+    function it_only_accepts_srt_files()
+    {
+        $fileGroup = $this->createFileGroup();
+
+        dispatch(
+            new CleanSrtJob($fileGroup, "{$this->testFilesStoragePath}TextFiles/three-cues.ass")
+        );
+
+        $this->assertTrue(StoredFile::count() === 1);
+
+        $textFileJob = FileJob::findOrFail(1);
+
+        $this->assertSame('messages.file_is_not_srt', $textFileJob->error_message);
+        $this->assertNotSame(null, $textFileJob->finished_at);
+        $this->assertSame(null, $textFileJob->output_stored_file_id);
+    }
+
+    /** @test */
+    function it_cleans_everything_by_default()
+    {
+        $fileGroup = $this->createFileGroup();
+
+        dispatch(
+            new CleanSrtJob($fileGroup, "{$this->testFilesStoragePath}TextFiles/three-cues-cleanable.srt")
+        );
+
+        $fileJob = $fileGroup->fileJobs()->firstOrFail();
+
+        $this->assertSame('three-cues-cleanable.srt', $fileJob->original_name);
+
+        $convertedStoredFile = $fileJob->outputStoredFile;
+
+        $subtitle = TextFileFormat::getMatchingFormat($convertedStoredFile->filePath);
+
+        $this->assertTrue($subtitle instanceof Srt);
+
+        // it removes the duplicate cue
+        $this->assertSame(3, count($subtitle->getCues()));
+
+        $this->assertNotContains('<i>', $subtitle->getContent());
+        $this->assertNotContains('</i>', $subtitle->getContent());
+        $this->assertNotContains('{', $subtitle->getContent());
+        $this->assertNotContains('}', $subtitle->getContent());
+    }
+
+    /** @test */
+    function job_options_can_disable_options()
+    {
+        $fileGroup = $this->createFileGroup();
+
+        $fileGroup->job_options = [
+          'stripCurly' => false,
+          'stripAngle' => false,
+        ];
+
+        $fileGroup->save();
+
+        dispatch(
+            new CleanSrtJob($fileGroup, "{$this->testFilesStoragePath}TextFiles/three-cues-cleanable.srt")
+        );
+
+        $fileJob = $fileGroup->fileJobs()->firstOrFail();
+
+        $this->assertSame('three-cues-cleanable.srt', $fileJob->original_name);
+
+        $convertedStoredFile = $fileJob->outputStoredFile;
+
+        $subtitle = TextFileFormat::getMatchingFormat($convertedStoredFile->filePath);
+
+        $this->assertTrue($subtitle instanceof Srt);
+
+        // it still removes the duplicate cue
+        $this->assertSame(3, count($subtitle->getCues()));
+
+        $this->assertContains('<i>', $subtitle->getContent());
+        $this->assertContains('</i>', $subtitle->getContent());
+        $this->assertContains('{', $subtitle->getContent());
+        $this->assertContains('}', $subtitle->getContent());
+    }
+}
