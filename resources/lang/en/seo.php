9856eb71995d9f7bfca26bfa59d4573ffa2a59e6
Sjors <sjorsottjes@gmail.com>
2017-08-25 10:14:31 +0200

    fill in seo

--- resources/lang/en/seo.php
+++ resources/lang/en/seo.php
@@ -2,21 +2,47 @@
 
 return [
 
-    'title.convertToSrt' => 'Convert Subtitles to Srt: Online Conversion Tool | Subtitle Tools',
-    'title.cleanSrt' => '',
-    'title.shift' => '',
-    'title.shiftPartial' => '',
-    'title.convertToUtf8' => '',
-    'title.pinyin' => '',
-    'title.subIdx' => '',
-    'title.subIdxDetail' => '',
-    'title.home' => '',
-    'title.contact' => '',
-    'title.fileGroupResult' => '',
+// SEO guidelines
+//    Page title recommended length: 60 characters (or 600px)
+//    Description max length: 160 characters
 
-    'description.convertToSrt' => 'Convert subtitles to srt online for free. This tool can convert ass to srt, ssa to srt and smi to srt. Upload zip file to convert a batch to srt. Works on Windows, Mac and Linux',
+//---------------60-characters-'############################################################'---------------------------
+    'title.cleanSrt'        => 'Srt Cleaner: Online Srt Clean and Fix Tool | Subtitle Tools',
+    'title.contact'         => 'Contact: Send us Feedback | Subtitle Tools',
+    'title.convertToSrt'    => 'Convert Subtitles to Srt: Online Conversion Tool | Subtitle Tools',
+    'title.convertToUtf8'   => 'Convert to UTF-8: Online Text File Encoding Fix | Subtitle Tools',
+    'title.fileGroupResult' => 'Download | Subtitle Tools',
+    'title.home'            => 'Subtitle Tools - Online Tools to Sync, Convert and Fix Subtitles',
+    'title.pinyin'          => 'Online Chinese Pinyin Subtitles Generator | Subtitle Tools',
+    'title.shift'           => 'Subtitle Sync Shifter: Online Subtitle Resync | Subtitle Tools',
+    'title.shiftPartial'    => 'Partial Subtitle Shifter: Sync Multiple Parts | Subtitle Tools',
+    'title.subIdx'          => 'Convert Sub/Idx to Srt: Online Conversion Tool | Subtitle Tools',
+    'title.subIdxDetail'    => 'Download Sub/Idx | Subtitle Tools',
+
+//--------------160-characters-------'################################################################################################################################################################'---------------------------
+    'description.cleanSrt'        => 'Removes formatting, brackets (curly, parentheses or angle brackets), html and other effects from subrip files. Easy online tool for cleaning srt subtitles',
+    'description.contact'         => 'We appreciate all feedback! Contact us if you have any suggestions, or if you want to report a bug',
+    'description.convertToSrt'    => 'Convert subtitles to srt online for free. This tool can convert ass to srt, ssa to srt and smi to srt. Upload zip file to convert a batch to srt. Works on Windows, Mac and Linux',
+    'description.convertToUtf8'   => 'Easily convert text or subtitle files to UTF-8. Supports all text files (txt, srt, ascii). This tool automatically detects the encoding and converts it to UTF-8',
+    'description.fileGroupResult' => 'Download processed files',
+    'description.home'            => 'Online tools for fixing subtitle files. Fast, easy, and no installation of freeware required. Tools for resyncing, cleaning and converting subtitle files. Supports most formats: srt, ass, sub/idx and smi',
+    'description.pinyin'          => 'Convert Chinese subtitles to pinyin, easily make pinyin subtitles online. Learn Chinese from movies and reading pinyin. Supports srt, ass and ssa files',
+    'description.shift'           => 'Synchronize subtitles online for free. Supports srt, ass, ssa and smi. Sync and fix subtitle timings by shifting them backwards or forwards',
+    'description.shiftPartial'    => 'This tools allows you to shift multiple points in a subtitle file. Resync specific parts of subtitles online. Supports srt, ass, ssa and smi files',
+    'description.subIdx'          => 'Convert sub/idx to srt online for free. Convert sub to srt without having to install freeware. Fast and free conversion of DVD subtitles. Works on Windows, Mac and Linux.',
+    'description.subIdxDetail'    => 'Download languages from sub/idx files',
 
 
-    'keywords.convertToSrt' => 'convert to srt, subtitles to srt, ass to srt, ssa to srt, smi to srt, ass2srt, ssa2srt, smi2srt, online, free, converter, conversion, subtitles, windows, mac, linux, freeware',
+    'keywords.cleanSrt'        => 'subtitle, subtitles, srt, clean, bracket, html, braces, angle, effects, colors, font, b, i',
+    'keywords.contact'         => 'contact, email, support, feedback',
+    'keywords.convertToSrt'    => 'convert to srt, subtitles to srt, ass to srt, ssa to srt, smi to srt, ass2srt, ssa2srt, smi2srt, online, free, converter, conversion, subtitles, windows, mac, linux, freeware',
+    'keywords.convertToUtf8'   => 'subtitle, subtitles, srt, ass, txt, utf-8, encoding, file, files, text, textfile, convert',
+    'keywords.fileGroupResult' => 'download, processed, files, subtitles, subtitle',
+    'keywords.home'            => 'subtitle, subtitles, tool, tools, convert, clean, srt, ass, smi, ssa',
+    'keywords.pinyin'          => 'pinyin, subtitles, make, chinese, learn, subtitle, srt, ass, smi, taiwan, hong kong, china',
+    'keywords.shift'           => 'sync, resync, shift, shifter, online, free, tool, srt, ass, ssa, smi, fix subtitle, synchronizer',
+    'keywords.shiftPartial'    => 'sync, resync, shift, shifter, online, free, tool, srt, ass, ssa, smi, fix subtitle, synchronizer, multi, partial,',
+    'keywords.subIdx'          => 'sub, idx, sub/idx, sub to srt, idx to srt, sub/idx to srt, freeware, mac, linux, windows, online, free, fast, easy, convert, converter, dvd, subtitles, format, subrip, vobsub',
+    'keywords.subIdxDetail'    => 'download, processed, files, subtitles, subtitle, sub, idx, sub/idx',
 
 ];
