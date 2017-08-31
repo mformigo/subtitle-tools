46045952c47b34533c3aec71bfb0b53b484d8024
Sjors <sjorsottjes@gmail.com>
2017-08-31 16:36:15 +0200

    slightly change RedirectsProvider

--- app/Providers/RedirectsProvider.php
+++ app/Providers/RedirectsProvider.php
@@ -7,21 +7,22 @@ use Illuminate\Support\ServiceProvider;
 
 class RedirectsProvider extends ServiceProvider
 {
+    protected $redirects = [
+        // (old) url               // destination route name
+        'format-converter'         => 'convertToSrt',
+        'convert-to-srt'           => 'convertToSrt',
+        'fo...'                    => 'convertToSrt',
+        'tools'                    => 'home',
+        'chinese-to-pinyin'        => 'pinyin',
+        'subtitle-shift'           => 'shift',
+        'partial-subtitle-shifter' => 'shiftPartial',
+        'multi-subtitle-shift'     => 'shiftPartial',
+    ];
+
     public function boot()
     {
-        $redirects = [
-            'format-converter' => 'convertToSrt',
-            'convert-to-srt'   => 'convertToSrt',
-            'fo...'            => 'convertToSrt',
-            'tools'            => 'home',
-            'chinese-to-pinyin'        => 'pinyin',
-            'subtitle-shift'           => 'shift',
-            'partial-subtitle-shifter' => 'shiftPartial',
-            'multi-subtitle-shift'     => 'shiftPartial',
-        ];
-
-        foreach($redirects as $url => $destinationRouteName) {
-            Route::get($url, function() use ($destinationRouteName) {
+        foreach($this->redirects as $url => $destinationRouteName) {
+            Route::any($url, function() use ($destinationRouteName) {
                 return redirect(route($destinationRouteName), 301);
             });
         }
