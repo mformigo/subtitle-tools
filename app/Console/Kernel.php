7c0213a4f250f72c7d30b7c0abe4f1884f114665
Sjors <sjorsottjes@gmail.com>
2017-08-31 18:51:18 +0200

    update sitemap package

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('sitemap:generate')->dailyAt('13:00');
+        $schedule->command('sitemap:generate')->dailyAt('2:00');
     }
 
     /**
