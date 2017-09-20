af90a5645cb30fe064e91d776f692fbd7cbf4c76
Sjors <sjorsottjes@gmail.com>
2017-09-20 11:46:36 +0200

    fix footer style, fix hindi flag

--- resources/views/guest/layout/footer.blade.php
+++ resources/views/guest/layout/footer.blade.php
@@ -1,10 +1,12 @@
 <footer id="Footer">
     <div class="container">
 
-        <a class="feedback" title="{{ __('nav.item.contact') }}" href="{{ route('contact') }}">
-            Feedback
-            <i class="material-icons">message</i>
-        </a>
+        <div class="feedback-wrapper">
+            <a class="feedback" title="{{ __('nav.item.contact') }}" href="{{ route('contact') }}">
+                Feedback
+                <i class="material-icons">message</i>
+            </a>
+        </div>
 
     </div>
 </footer>
