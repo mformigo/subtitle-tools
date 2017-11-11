92c1d8aa810cf706b3ed6d5df02c2d392ebfe24e
Sjors <sjorsottjes@gmail.com>
2017-11-11 08:06:35 +0100

    improve anti adblock text

--- resources/views/helpers/anti-adblock-banner.blade.php
+++ resources/views/helpers/anti-adblock-banner.blade.php
@@ -2,8 +2,8 @@
     <div id="please-im-begging-you" class="hidden" aria-hidden="true">
         <img src="/images/adblock-logo.png" alt="Please disable your adblocker for this website">
         <span>
-            <strong>Could you disable adblock for this website?</strong>
-            I've put a lot of hours into making these tools, I would appreciate it if you could support me by disabling your adblocker
+            <strong>Could you whitelist this website in your adblocker?</strong>
+            I would appreciate it if you could support me by disabling your adblocker, I've put a lot of hours into making these tools
         </span>
     </div>
 </div>
@@ -14,11 +14,6 @@
             document.getElementById('please-im-begging-you').classList.remove("hidden");
         }
 
-        if(typeof blockAdBlock === 'undefined') {
-            adBlockDetected();
-        }
-        else {
-            blockAdBlock.onDetected(adBlockDetected);
-        }
+        (typeof blockAdBlock === 'undefined') ? adBlockDetected() : blockAdBlock.onDetected(adBlockDetected);
     </script>
 @endpush
