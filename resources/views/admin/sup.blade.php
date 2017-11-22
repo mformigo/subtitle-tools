da27d96622f2b2df46b353442f9e5cbb7729e064
Sjors <sjorsottjes@gmail.com>
2017-11-22 14:48:55 +0100

    improve sup dashboard

--- resources/views/admin/sup.blade.php
+++ resources/views/admin/sup.blade.php
@@ -7,8 +7,11 @@
 
         <div class="st-row header">
             <div class="st-col minw-50"></div>
+            <div class="st-col minw-100">Input</div>
             <div class="st-col minw-100">Output</div>
-            <div class="st-col minw-75">Ocr Lang</div>
+            <div class="st-col minw-100">Format</div>
+            <div class="st-col minw-75">Cue Count</div>
+            <div class="st-col minw-100">Ocr Lang</div>
             <div class="st-col st-grow">Size</div>
             <div class="st-col minw-75">Queue</div>
             <div class="st-col minw-75">Extract</div>
@@ -21,11 +24,14 @@
         @foreach($sups as $sup)
             <div class="st-row">
                 <div class="st-col minw-50"><a href="{{ route('sup.show', $sup->url_key) }}" target="_blank">&nbsp;(*)</a></div>
+                <div class="st-col minw-100">{{ $sup->input_stored_file_id }}</div>
                 <div class="st-col minw-100">
                     @if($sup->output_stored_file_id)
                         <a target="_blank" href="{{ route('adminStoredFileDetail', $sup->output_stored_file_id) }}">{{ $sup->output_stored_file_id }}</a>
                     @endif
                 </div>
+                <div class="st-col minw-100">{{ optional($sup->meta)->format }}</div>
+                <div class="st-col minw-100">{{ optional($sup->meta)->cue_count }} cues</div>
                 <div class="st-col minw-75">{{ $sup->ocr_language }}</div>
                 <div class="st-col st-grow">{{ $sup->inputStoredFile->meta ? round($sup->inputStoredFile->meta->size / 1024, 0) : '' }}kb</div>
                 <div class="st-col minw-75">{{ $sup->queue_time }} s</div>
