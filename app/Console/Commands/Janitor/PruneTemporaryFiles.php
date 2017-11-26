f2203a07a5286f6ac496befb9127a703d9e6fa75
Sjors <sjorsottjes@gmail.com>
2017-11-26 12:37:42 +0100

    keep temporary files for five days

--- app/Console/Commands/Janitor/PruneTemporaryFiles.php
+++ app/Console/Commands/Janitor/PruneTemporaryFiles.php
@@ -8,29 +8,38 @@ class PruneTemporaryFiles extends Command
 {
     protected $signature = 'st:prune-temporary-files';
 
-    protected $description = 'Prune temporary files and directories older than one day';
+    protected $description = 'Prune temporary files and directories older than five days';
 
     public function handle()
     {
-        $this->info('Deleting temporary files and directories older than one day...');
+        $this->info('Deleting temporary files and directories older than five days...');
 
-        $prefixOfToday = date('Y-z') . '-';
+        // Slow OCR jobs could take more than a day if the queue is very busy,
+        // to be safe, only delete things older than five days
+        $dontDeletePrefixes = [
+            date('Y-z-'), // todays prefix
+            date('Y-z-', strtotime('-1 days')),
+            date('Y-z-', strtotime('-2 days')),
+            date('Y-z-', strtotime('-3 days')),
+            date('Y-z-', strtotime('-4 days')),
+        ];
 
-        $this->cleanTemporaryFiles($prefixOfToday);
 
-        $this->cleanTemporaryDirectories($prefixOfToday);
+        $this->cleanTemporaryFiles($dontDeletePrefixes);
+
+        $this->cleanTemporaryDirectories($dontDeletePrefixes);
 
         $this->call('st:calculate-disk-usage');
     }
 
-    protected function cleanTemporaryFiles($prefixOfToday)
+    protected function cleanTemporaryFiles($dontDeletePrefixes)
     {
         $temporaryFilesDir = storage_disk_file_path('temporary-files/');
 
         $fileNames = scandir($temporaryFilesDir);
 
-        $fileNames = array_filter($fileNames, function ($name) use ($prefixOfToday) {
-            return ! starts_with($name, ['.', $prefixOfToday]);
+        $fileNames = array_filter($fileNames, function ($name) use ($dontDeletePrefixes) {
+            return ! starts_with($name, '.') && ! starts_with($name, $dontDeletePrefixes);
         });
 
         foreach ($fileNames as $name) {
@@ -38,14 +47,19 @@ class PruneTemporaryFiles extends Command
         }
     }
 
-    protected function cleanTemporaryDirectories($prefixOfToday)
+    protected function cleanTemporaryDirectories($dontDeletePrefixes)
     {
         $temporaryDirectoriesDirectory = storage_disk_file_path('temporary-dirs/');
 
         $dirPaths = glob($temporaryDirectoriesDirectory.'*', GLOB_ONLYDIR);
 
-        $dirPaths = array_filter($dirPaths, function ($name) use ($prefixOfToday) {
-            return ! str_contains($name, DIRECTORY_SEPARATOR.$prefixOfToday);
+        $dirPaths = array_filter($dirPaths, function ($name) use ($dontDeletePrefixes) {
+            foreach($dontDeletePrefixes as $prefix) {
+                if(str_contains($name, DIRECTORY_SEPARATOR.$prefix)) {
+                    return false;
+                }
+            }
+            return true;
         });
 
         foreach($dirPaths as $path) {
