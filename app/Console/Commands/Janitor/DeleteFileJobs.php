b21392e657e661130a8ce5ff45c40a1382fc0998
Sjors <sjorsottjes@gmail.com>
2017-11-28 19:58:56 +0100

    update delete file jobs command

--- app/Console/Commands/Janitor/DeleteFileJobs.php
+++ app/Console/Commands/Janitor/DeleteFileJobs.php
@@ -3,7 +3,6 @@
 namespace App\Console\Commands\Janitor;
 
 use App\Models\FileGroup;
-use App\Models\FileJob;
 use Illuminate\Console\Command;
 
 class DeleteFileJobs extends Command
@@ -14,22 +13,9 @@ class DeleteFileJobs extends Command
 
     public function handle()
     {
-        $this->info('Truncating file jobs and file groups...');
-
-        if(! app()->isDownForMaintenance()) {
-            $this->error('This command can only be run in maintenance mode');
-
-            return;
-        }
+        $this->info('Truncating file groups...');
 
         // The left-over stored files are deleted daily by the PruneStoredFiles command
-
-        FileJob::truncate();
-
         FileGroup::truncate();
-
-        $this->info('Don\'t forget to run php artisan up!');
-        $this->info('Don\'t forget to run php artisan up!!');
-        $this->info('Don\'t forget to run php artisan up!!!');
     }
 }
