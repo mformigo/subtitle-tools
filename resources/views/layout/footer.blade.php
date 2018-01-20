71ccd9adddefa634403ad6342d25638a628800e7
Sjors <sjorsottjes@gmail.com>
2018-01-20 13:52:23 +0100

    add cryto donation buttons

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,15 +2,40 @@
     <div class="container mx-auto px-4 flex sm:flex-row flex-col justify-between flex-wrap text-center">
 
         <div class="flex flex-col">
-            <span class="mb-2">
+            <span class="mb-4">
                 Support me and the site
             </span>
-            <form class="mx-auto" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+
+            <form class="mx-auto mb-4" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYANN+XPeSx2Ze2e8BAeKpK7lIvzL1wQFjIOhCIbAOa6MDDLBLvYXAiCF4i459mqxdK6hmfXg+ue30O24Ikt1xyoDLiKiwYI5egAShsb52VCHMnhvesez7ovefFFWymEwAI2KsRlmvnyaoUtJV2eEe3E0TgAjMQzIQM+imRYm08QrjELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIuBFznyo4qEOAgZAE/XZnRN2xLqRczhqmjl3uhu5DNCQ+EZBhQQ6HBMR/J9o61DKZZx7d48BXSZB1zlTmnPt6054/9bJs1IsVMl5TSa3JCtcVDfqzaj1yTKx9w1AN91AwKZEqPOd0OYk/j2GhnH/JdYJGbSZ4mNOE2iQOmvXIDjC/IXO2vR3OprynhbN4YYkyM7lq3nmy/Bx76lagggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNzExMjUxNjM2NDBaMCMGCSqGSIb3DQEJBDEWBBQJSkZnXasRUPgf+UXecGkqR06YiTANBgkqhkiG9w0BAQEFAASBgGjYZnIrg/r6GlxFFsMU1EOTAC8xbKrN3YpPN9ttEuTdScoiws9HwU+BTdjRNem6R6/5k42fHEnEZZR4MMf6Phe/CgganI1pa8DnNcjTg2NCnhSW3bih4q9RElhSY9o47fjD0ivPKVuiSBYiqjMhhM3CYilVK6+H+XW4BdalXw/u-----END PKCS7-----">
                 <input class="cursor-pointer" title="Donate using PayPal" type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                 <img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
             </form>
+
+            <div class="flex items-center justify-around">
+
+                <div class="w-8">
+                    <a href="https://blockchain.info/address/1MSGtvPJ5nrtdqhsmnn4Fb6KjmkySLNGot" title="Donate Bitcoin (BTC)" target="_blank" rel="nofollow">
+                        @include('helpers.svg.bitcoin')
+                    </a>
+                </div>
+
+                <div class="w-6">
+                    <a href="https://etherscan.io/address/0xC70e876b14eB5878Fe7Ae85431758c5b855902Fc" title="Donate Ethereum (ETH)" target="_blank" rel="nofollow">
+                        @include('helpers.svg.ethereum')
+                    </a>
+                </div>
+
+
+                <div class="w-8">
+                    <a href="https://live.blockcypher.com/ltc/address/LcQrB8CYXwaxhfzomaCvVYyxqPwVNbzuq7/" title="Donate Litecoin (LTC)" target="_blank" rel="nofollow">
+                        @include('helpers.svg.litecoin')
+                    </a>
+                </div>
+
+            </div>
+
         </div>
 
         <div class="sm:mt-0 mt-4">
