f77e4fa22cffd6a75857a4577b3595f29f49ff6d
Sjors <sjorsottjes@gmail.com>
2018-02-17 14:25:55 +0100

    validate merge tool base file

--- app/Http/Controllers/FileJobs/MergeController.php
+++ app/Http/Controllers/FileJobs/MergeController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\FileJobs;
 use App\Http\Rules\FileNotEmptyRule;
 use App\Http\Rules\SubtitleFileRule;
 use App\Jobs\FileJobs\MergeSubtitlesJob;
+use App\Subtitles\ContainsGenericCues;
 use App\Subtitles\Tools\Options\MergeSubtitlesOptions;
+use App\Support\Facades\TextFileFormat;
 
 class MergeController extends FileJobController
 {
@@ -30,7 +32,20 @@ class MergeController extends FileJobController
     protected function rules(): array
     {
         return [
-            'subtitles' => ['required', 'file', new FileNotEmptyRule, new SubtitleFileRule],
+            'subtitles' => [
+                'bail',
+                'required',
+                'file',
+                new FileNotEmptyRule,
+                new SubtitleFileRule,
+                function ($attribute, $value, $fail) {
+                    $inputSubtitle = TextFileFormat::getMatchingFormat($value->getRealPath(), false);
+
+                    return $inputSubtitle instanceof ContainsGenericCues
+                        ? null
+                        : $fail('The base subtitle format is not supported');
+                },
+            ],
         ];
     }
 }
