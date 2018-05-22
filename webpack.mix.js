f4a2e09043f0cfa9f6393be68013089ffb18b960
Sjors <sjorsottjes@gmail.com>
2018-05-22 20:27:19 +0200

    update npm dependencies, clean up tailwind config

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,42 +1,20 @@
 let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
-let glob = require('glob-all');
-let PurgecssPlugin = require('purgecss-webpack-plugin');
+require('laravel-mix-purgecss');
 
-mix.disableNotifications();
-
-
-
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.disableNotifications()
+   .js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/admin.scss', 'public/css')
    .postCss('resources/assets/pcss/main.pcss', 'public/css', [
         tailwindcss('tailwind.js')
-    ]);
+    ])
+    .purgeCss({
+        globs: [
+            path.join(__dirname, 'resources/views/**/*.php'),
+            path.join(__dirname, 'resources/assets/js/**/*.js'),
+            path.join(__dirname, 'resources/assets/js/**/*.vue'),
+        ],
 
-mix.version();
-
-if (mix.inProduction()) {
-    mix.webpackConfig({
-        plugins: [
-            new PurgecssPlugin({
-
-                paths: glob.sync([
-                    path.join(__dirname, 'resources/views/**/*.php'),
-                    path.join(__dirname, 'resources/assets/js/**/*.js'),
-                    path.join(__dirname, 'resources/assets/js/**/*.vue')
-                ]),
-                extractors: [
-                    {
-                        extractor: class {
-                            static extract(content) {
-                                return content.match(/[A-z0-9-:\/]+/g) || []
-                            }
-                        },
-
-                        extensions: ['html', 'js', 'php', 'vue']
-                    }
-                ]
-            })
-        ]
-    });
-}
+        extensions: ['html', 'js', 'php', 'vue'],
+    })
+    .version();
