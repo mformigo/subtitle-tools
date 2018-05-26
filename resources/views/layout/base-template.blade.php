7d182f1e38c02887fbda5a0cb541deb60bb4adea
Sjors <sjorsottjes@gmail.com>
2018-05-26 09:06:31 +0200

    defer ads

--- resources/views/layout/base-template.blade.php
+++ resources/views/layout/base-template.blade.php
@@ -30,7 +30,7 @@
         <link rel="stylesheet" type="text/css" href="{{ mix('css/main.css') }}" />
 
         @if(App::environment('production'))
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <script async defer src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({
                     google_ad_client: "ca-pub-8027891891391991",
