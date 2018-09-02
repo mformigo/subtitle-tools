1bb80c0c6bdb91ec849e58770235493a7c05be71
Sjors <sjorsottjes@gmail.com>
2018-09-02 12:39:00 +0200

    update footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -4,37 +4,37 @@
         <div class="flex flex-col">
             <span class="mb-6">
                 <strong class="block">Donate</strong>
-                <span class="block w-48 sm:mx-0 mx-auto">Support me and the site with a small donation</span>
+                <span class="block w-48 sm:mx-0 mx-auto">Help keep this site online</span>
             </span>
 
             <form class="mx-auto mb-4" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHNwYJKoZIhvcNAQcEoIIHKDCCByQCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYANN+XPeSx2Ze2e8BAeKpK7lIvzL1wQFjIOhCIbAOa6MDDLBLvYXAiCF4i459mqxdK6hmfXg+ue30O24Ikt1xyoDLiKiwYI5egAShsb52VCHMnhvesez7ovefFFWymEwAI2KsRlmvnyaoUtJV2eEe3E0TgAjMQzIQM+imRYm08QrjELMAkGBSsOAwIaBQAwgbQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQIuBFznyo4qEOAgZAE/XZnRN2xLqRczhqmjl3uhu5DNCQ+EZBhQQ6HBMR/J9o61DKZZx7d48BXSZB1zlTmnPt6054/9bJs1IsVMl5TSa3JCtcVDfqzaj1yTKx9w1AN91AwKZEqPOd0OYk/j2GhnH/JdYJGbSZ4mNOE2iQOmvXIDjC/IXO2vR3OprynhbN4YYkyM7lq3nmy/Bx76lagggOHMIIDgzCCAuygAwIBAgIBADANBgkqhkiG9w0BAQUFADCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wHhcNMDQwMjEzMTAxMzE1WhcNMzUwMjEzMTAxMzE1WjCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20wgZ8wDQYJKoZIhvcNAQEBBQADgY0AMIGJAoGBAMFHTt38RMxLXJyO2SmS+Ndl72T7oKJ4u4uw+6awntALWh03PewmIJuzbALScsTS4sZoS1fKciBGoh11gIfHzylvkdNe/hJl66/RGqrj5rFb08sAABNTzDTiqqNpJeBsYs/c2aiGozptX2RlnBktH+SUNpAajW724Nv2Wvhif6sFAgMBAAGjge4wgeswHQYDVR0OBBYEFJaffLvGbxe9WT9S1wob7BDWZJRrMIG7BgNVHSMEgbMwgbCAFJaffLvGbxe9WT9S1wob7BDWZJRroYGUpIGRMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbYIBADAMBgNVHRMEBTADAQH/MA0GCSqGSIb3DQEBBQUAA4GBAIFfOlaagFrl71+jq6OKidbWFSE+Q4FqROvdgIONth+8kSK//Y/4ihuE4Ymvzn5ceE3S/iBSQQMjyvb+s2TWbQYDwcp129OPIbD9epdr4tJOUNiSojw7BHwYRiPh58S1xGlFgHFXwrEBb3dgNbMUa+u4qectsMAXpVHnD9wIyfmHMYIBmjCCAZYCAQEwgZQwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tAgEAMAkGBSsOAwIaBQCgXTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xNzExMjUxNjM2NDBaMCMGCSqGSIb3DQEJBDEWBBQJSkZnXasRUPgf+UXecGkqR06YiTANBgkqhkiG9w0BAQEFAASBgGjYZnIrg/r6GlxFFsMU1EOTAC8xbKrN3YpPN9ttEuTdScoiws9HwU+BTdjRNem6R6/5k42fHEnEZZR4MMf6Phe/CgganI1pa8DnNcjTg2NCnhSW3bih4q9RElhSY9o47fjD0ivPKVuiSBYiqjMhhM3CYilVK6+H+XW4BdalXw/u-----END PKCS7-----">
-                <input class="cursor-pointer max-h-8" title="Donate using PayPal" type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                <input class="cursor-pointer h-6" title="Donate using PayPal" type="image" src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                 <img alt="" border="0" src="https://www.paypalobjects.com/nl_NL/i/scr/pixel.gif" width="1" height="1">
             </form>
 
             <div class="flex items-center justify-around">
 
-                <div class="w-8">
+                <div class="w-6">
                     <a href="https://blockchain.info/address/1MSGtvPJ5nrtdqhsmnn4Fb6KjmkySLNGot" title="Donate Bitcoin (BTC)" target="_blank" rel="nofollow">
                         @include('helpers.svg.bitcoin')
                     </a>
                 </div>
 
-                <div class="w-6">
+                <div class="w-4">
                     <a href="https://etherscan.io/address/0xC70e876b14eB5878Fe7Ae85431758c5b855902Fc" title="Donate Ethereum (ETH)" target="_blank" rel="nofollow">
                         @include('helpers.svg.ethereum')
                     </a>
                 </div>
 
-                <div class="w-8">
+                <div class="w-6">
                     <a href="https://live.blockcypher.com/ltc/address/LcQrB8CYXwaxhfzomaCvVYyxqPwVNbzuq7/" title="Donate Litecoin (LTC)" target="_blank" rel="nofollow">
                         @include('helpers.svg.litecoin')
                     </a>
                 </div>
 
-                <div class="w-10">
+                <div class="w-8">
                     <a href="https://explorer.bitcoin.com/bch/address/bitcoincash:qpwznnur9d8eedcc6gfyftf32azaj0lq7s82cm4kyz" title="Donate Bitcoin Cash (BCH)" target="_blank" rel="nofollow">
                         @include('helpers.svg.bitcoin-cash')
                     </a>
@@ -45,9 +45,16 @@
         </div>
 
         <div class="flex flex-col sm:mt-0 mt-4 sm:text-right text-center">
-            <a class="mb-2 text-black hover:underline" href="{{ route('contact') }}">Contact / Feedback / Requests</a>
+            <a class="mb-2 text-black hover:underline" href="{{ route('contact') }}">Contact, feedback or requests</a>
 
-            <a class="mb-2 text-black hover:underline" href="{{ route('stats') }}">Statistics</a>
+            <span class="mb-2">
+                Created by
+                <a class="text-black hover:underline" target="_blank" href="https://twitter.com/SjorsOttjes">Sjors Ottjes</a>
+            </span>
+
+            <span class="mb-2">
+                <a class="text-black hover:underline" href="{{ route('stats') }}">Statistics</a>
+            </span>
         </div>
 
     </div>
