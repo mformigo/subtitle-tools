53a888c47bace19dce8e6c2a2b6bcd04074ca5c4
Sjors <sjorsottjes@gmail.com>
2019-02-09 12:41:43 +0100

    wip

--- database/migrations/2019_02_09_112120_add_missing_foreign_key.php
+++ database/migrations/2019_02_09_112120_add_missing_foreign_key.php
@@ -18,20 +18,20 @@ class AddMissingForeignKey extends Migration
 //                $table->foreign('output_stored_file_id')->references('id')->on('stored_files');
 //            });
 
-            Schema::table('stored_files', function (Blueprint $table) {
-                $table->unique('hash');
-            });
-
-            Schema::table('file_groups', function (Blueprint $table) {
-                $table->foreign('archive_stored_file_id')->references('id')->on('stored_files');
-            });
-
-            Schema::table('sup_jobs', function (Blueprint $table) {
-                $table->foreign('input_stored_file_id')->references('id')->on('stored_files');
-                $table->foreign('output_stored_file_id')->references('id')->on('stored_files');
-
-                $table->unique(['ocr_language', 'input_file_hash']);
-            });
+//            Schema::table('stored_files', function (Blueprint $table) {
+//                $table->unique('hash');
+//            });
+//
+//            Schema::table('file_groups', function (Blueprint $table) {
+//                $table->foreign('archive_stored_file_id')->references('id')->on('stored_files');
+//            });
+//
+//            Schema::table('sup_jobs', function (Blueprint $table) {
+//                $table->foreign('input_stored_file_id')->references('id')->on('stored_files');
+//                $table->foreign('output_stored_file_id')->references('id')->on('stored_files');
+//
+//                $table->unique(['ocr_language', 'input_file_hash']);
+//            });
 
             Schema::table('sup_job_metas', function (Blueprint $table) {
                 $table->unique('sup_job_id');
